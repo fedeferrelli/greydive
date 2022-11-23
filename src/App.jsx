527e401ed69fc45fab26adb0684229717ba44f80
Fede <ferrelli.mazza@gmail.com>
2022-11-23 17:28:32 -0300

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div >
+      <h1>greydive challenge</h1>
+      <a href="https://portafolio-fedeferrelli.vercel.app/" target="_blank">Fede Mazza</a>
+    </div>
+  )
+}
+
+export default App
